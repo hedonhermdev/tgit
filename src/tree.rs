e0a05d50d414f9cb57b8b8d3dc2063287f704d64
Tirth Jain <jaintirth24@gmail.com>
2020-08-20 00:29:15 +0530

    Refactor

--- src/tree.rs
+++ src/tree.rs
@@ -3,7 +3,7 @@ use std::io::{Read, BufRead, Cursor};
 use std::path::PathBuf;
 use crate::utils;
 use std::fmt;
-use std::fmt::splay;
+use std::fmt::Display;
 
 #[derive(Debug, Clone)]
 pub struct Tree {
